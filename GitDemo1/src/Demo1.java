53124ce9802b8e108194cebb7e2bddd61958ec0c
MANISH <MANISH@DESKTOP-NPV50CU>
2021-04-19 11:57:52 +0530

    Submitted By Manish

---
+++ GitDemo1/src/Demo1.java
@@ -0,0 +1,18 @@
+
+public class Demo1
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Hello Demo Testing01 ");
+
+	}
+	
+	public void multiply()
+	{
+		System.out.println(" Hello multiply");
+	}
+
+}
